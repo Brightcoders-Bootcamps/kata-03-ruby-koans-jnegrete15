14afc6e8077d0a0ae081b24f92dafb2b29b12f19
Jose Alberto Negrete Gonzalez <jnegrete15@ucol.mx>
2021-01-09 16:02:15 -0600

    konas about constants, control statements and dice project

--- rubykoans/koans/about_dice_project.rb
+++ rubykoans/koans/about_dice_project.rb
@@ -2,9 +2,16 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+ class DiceSet
+  attr_reader :values
+
+  def initialize()
+    @values = []
+  end
+   def roll(tamaño)
+    @values = Array.new(tamaño) { |x| x = rand(1..6) }
+   end
+ end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
